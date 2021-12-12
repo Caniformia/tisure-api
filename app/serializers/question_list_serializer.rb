6664945a7808290d1575bd2b418783b05b363b54
DDoSolitary <DDoSolitary@gmail.com>
2021-12-12 14:56:54 +0800

    add count to question list

--- app/serializers/question_list_serializer.rb
+++ app/serializers/question_list_serializer.rb
@@ -1,6 +1,10 @@
 class QuestionListSerializer < ActiveModel::Serializer
-  attributes :id, :name, :visibility, :share_link
+  attributes :id, :name, :visibility, :share_link, :question_count
   has_one :owner
   has_one :forked_from
   has_many :question_list_items
+
+  def question_count
+    object.question_list_items.count
+  end
 end
