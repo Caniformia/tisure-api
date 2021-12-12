308000358712ac103c85c9b8b9b3a7aa347eb77a
DDoSolitary <DDoSolitary@gmail.com>
2021-12-12 15:31:11 +0800

    add total count to progress

--- app/serializers/chapter_progress_serializer.rb
+++ app/serializers/chapter_progress_serializer.rb
@@ -1,8 +1,12 @@
 class ChapterProgressSerializer < ActiveModel::Serializer
-  attributes :progress_count, :updated_at
+  attributes :progress_count, :total_count, :updated_at
   has_one :last_accessed_question
 
   def progress_count
     object.user.records.joins(:question).where('questions.chapter_id' => object.chapter_id).distinct.count(:question_id)
+  end
+
+  def total_count
+    object.chapter.questions.count
   end
 end
--- app/serializers/subject_progress_serializer.rb
+++ app/serializers/subject_progress_serializer.rb
@@ -1,9 +1,13 @@
 class SubjectProgressSerializer < ActiveModel::Serializer
-  attributes :progress_count, :updated_at
+  attributes :progress_count, :total_count, :updated_at
   has_one :last_accessed_chapter
 
   def progress_count
     object.user.records.joins(question: [:chapter])
       .where('chapters.subject_id' => object.subject.id).distinct.count(:question_id)
+  end
+
+  def total_count
+    Question.joins(:chapter).where('chapters.subject_id' => object.subject.id).count
   end
 end
