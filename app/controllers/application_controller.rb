b302dd04b0082b4596b594ba5429d3ab17d71e08
DDoSolitary <DDoSolitary@gmail.com>
2021-12-12 12:55:20 +0800

    add user nickname

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
+    end
+
     def record_stats(records)
       return {
         all_users: {
