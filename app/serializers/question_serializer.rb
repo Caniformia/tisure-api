a8607b479263c7fdc9f9ac293b494cdd8dbf4650
DDoSolitary <DDoSolitary@gmail.com>
2021-12-12 15:46:31 +0800

    show question's belonging list

--- app/serializers/question_serializer.rb
+++ app/serializers/question_serializer.rb
@@ -1,9 +1,13 @@
 class QuestionSerializer < ActiveModel::Serializer
-  attributes :id, :content, :is_multi_answer, :chapter_id, :image_url
+  attributes :id, :content, :is_multi_answer, :chapter_id, :image_url, :question_lists
   has_many :choices
   has_many :comments
 
   def image_url
     Rails.application.routes.url_helpers.rails_blob_path(object.image, only_path: true) if object.image.attached?
+  end
+
+  def question_lists
+    object.question_lists.where owner: scope if scope.is_a? User
   end
 end
